6bc39447d7de844be6834e1b031d06dc942a5da9
ifotn <Rich.Freeman@GeorgianCollege.ca>
2024-01-29 10:45:27 -0500

    first commit

---
+++ Server/Controllers/index.js
@@ -0,0 +1,32 @@
+const User = require('../Models/user');
+
+/**
+ * This function will display the home page
+ *
+ * @export
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
+function DisplayHome(req, res, next)
+{
+  /* No need to create a User, but demonstrating how to use the Model */
+  let user = new User();
+  user.username = 'admin';
+  console.log(`username: ${user.username}`);
+
+  /* Now Render the ejs page */
+  res.render('index', {title: 'Home', page: 'home'});
+}
+
+function DisplayAbout(req, res, next)
+{
+  res.render('about', {title: 'About', page: 'about'});
+}
+
+module.exports = {
+ DisplayHome: DisplayHome,
+ DisplayAbout: DisplayAbout
+}
+
+  
---
+++ Server/Routes/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+
+// import the index controller
+const IndexController = require('../Controllers/index');
+
+/* GET Default Route */
+router.get('/', (req, res, next)=>
+{
+    IndexController.DisplayHome(req, res, next);
+});
+
+/* GET Home Page */
+router.get('/home', (req, res, next)=>
+{
+    IndexController.DisplayHome(req, res, next);
+});
+
+/* GET About Page */
+router.get('/about', (req, res, next)=>
+{
+    IndexController.DisplayAbout(req, res, next);
+});
+
+module.exports = router;
